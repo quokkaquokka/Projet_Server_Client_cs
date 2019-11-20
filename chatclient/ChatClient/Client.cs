eb8fb4c0b0780ace9edce936e1edb7be80f6655b
Théo <t.abdmeziem@gmail.com>
2019-11-20 15:14:21 +0100

    Configuration des deux projets client et server

---
+++ chatclient/ChatClient/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient {
+	class Client {
+		private TcpClient sock;
+		public Client() {
+			byte[] local_IP = { 127, 0, 0, 1 };
+
+			Console.WriteLine("Connecting...");
+			sock = new TcpClient("localhost", 8080);
+			// now talk with the server if connected
+
+			Console.WriteLine("Connected!!!");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ chatserver/ChatServer/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer {
+	class Server {
+		private TcpListener ear;
+		private TcpClient cli_sock;
+
+		public Server() {
+			byte[] local_IP = { 127, 0, 0, 1 };
+			ear = new TcpListener(new IPAddress(local_IP), 8080);
+
+			// start listening for client requests
+			ear.Start();
+
+			Console.WriteLine("Waiting for connection...");
+			cli_sock = ear.AcceptTcpClient(); // blocking
+
+			Console.WriteLine("Connection detected!!!");
+
+			// now talk with the client
+			Console.ReadLine();
+		}
+	}
+}
