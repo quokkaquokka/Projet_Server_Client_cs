e57eadc15928dfb49b2e7345a0fcf54e08ac2f2a
Théo <t.abdmeziem@gmail.com>
2019-12-04 16:50:00 +0100

    Added the receiver and clientManager files

---
+++ chatclient/ChatClient/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient {
+	class Receiver {
+		private StreamReader sIn;
+		private string msg;
+
+		public Receiver(TcpClient serverSocket) {
+			sIn = new StreamReader(serverSocket.GetStream());
+		}
+
+		public void run() {
+			// wait for messages to display
+			while(true) {
+				msg = sIn.ReadLine();
+				Console.WriteLine(msg);
+			}
+		}
+	}
+}
---
+++ chatserver/ChatServer/ClientManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer {
+	class ClientManager {
+		private TcpClient clientSocket;
+		private StreamWriter sOut;
+		private StreamReader sIn;
+		private string msg;
+
+		public ClientManager(TcpClient clientSocket) {
+			this.clientSocket = clientSocket;
+			sOut = new StreamWriter(clientSocket.GetStream());
+			sOut.AutoFlush = true;
+
+			sIn = new StreamReader(clientSocket.GetStream());
+
+			msg = "";
+		}
+
+		public void run() {
+			// send a message to the new client
+			sOut.WriteLine("Welcome {name of the subject}, from {city of the subject}");
+
+			msg = sIn.ReadLine();
+			while(msg != "/q") {
+				Console.WriteLine(msg);
+				// apply the action depending on the message
+				msg = sIn.ReadLine();
+			}
+		}
+	}
+}
